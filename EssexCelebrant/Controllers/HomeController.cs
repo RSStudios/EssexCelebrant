ab1fd95786d182fd8ec1c86874aafc13011f32c8
Robert Silver <rs@rsstudios.co.uk>
2018-08-26 18:21:48 +0100

    FAQ marker point.  Taking out Bootstrap.

--- EssexCelebrant/Controllers/HomeController.cs
+++ EssexCelebrant/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace EssexCelebrant.Controllers
 {
@@ -24,6 +20,11 @@ namespace EssexCelebrant.Controllers
         {
             ViewBag.Message = "Contact Me";
 
+            return View();
+        }
+
+        public ActionResult Testimonials()
+        {
             return View();
         }
     }
