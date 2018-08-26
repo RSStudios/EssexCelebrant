9a840cc2e122b2c2e529c03bc443767b75c14c37
Robert Silver <rs@rsstudios.co.uk>
2018-08-26 17:09:07 +0100

    Marker point 2

---
+++ EssexCelebrant/Controllers/FuneralsController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace EssexCelebrant.Controllers
+{
+    public class FuneralsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
