968c4ceb49e360da555637921cdfab290d8227cd
Robert Silver <rs@rsstudios.co.uk>
2018-08-27 18:06:34 +0100

    Menu marker point.

--- EssexCelebrant/Controllers/FAQController.cs
+++ EssexCelebrant/Controllers/FAQController.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace EssexCelebrant.Controllers
 {
     public class FAQController : Controller
     {
-        // GET: FAQ
         public ActionResult Index()
         {
             return View("FAQ");
