e30989ed956c84c19dc4cc02e14e092a68a563f8
Robert Silver <rs@rsstudios.co.uk>
2018-09-18 13:24:27 +0100

    Added a blog controller to the page.

---
+++ EssexCelebrant/Controllers/BlogController.cs
@@ -0,0 +1,23 @@
+﻿using System.ServiceModel.Syndication;
+using System.Web.Mvc;
+using System.Xml;
+
+namespace EssexCelebrant.Controllers
+{
+    public class BlogController : Controller
+    {
+        public ActionResult Index()
+        {
+            string url = "http://snowprints.blogspot.com/feeds/posts/default";
+            XmlReader reader = XmlReader.Create(url);
+            SyndicationFeed feed = SyndicationFeed.Load(reader);
+            reader.Close();
+            foreach (SyndicationItem item in feed.Items)
+            {
+                string subject = item.Title.Text;
+                string summary = item.Summary.Text;
+            }
+            return View();
+        }
+    }
+}
