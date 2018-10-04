74dbaa74e65a97f20a5bbdb15f74688141827a3e
Robert Silver <rs@rsstudios.co.uk>
2018-10-04 20:52:29 +0100

    Still working on venues.

--- EssexCelebrant/Content/Scripts/App/vm.venue.js
+++ EssexCelebrant/Content/Scripts/App/vm.venue.js
@@ -4,25 +4,37 @@
 
     EssexCelebrant.VenuesVM = function (venueData) {
         var self = this;
-        self.venues = ko.observableArray(venueData);
+        self.masterVenues = ko.observableArray(venueData);
+        self.filteredVenues = ko.observableArray(venueData);
 
         self.clearSearchFilters = function () {
-         
+
         };
 
         self.runAdvancedSearch = function () {
 
         };
 
-        self.getAdvancedSearchMetaData = function () {
-           
+        self.searchForText = function () {
+            var searchTerm = this.value;
+            if (searchTerm === "") {
+                self.filteredVenues(self.masterVenues());
+            }
+            else {
+                self.filteredVenues(self.masterVenues().filter(allVenues => {
+                    if (allVenues.county.toLowerCase().includes(searchTerm)) {
+                        return true;
+                    }
+                    else if (allVenues.venue.toLowerCase().includes(searchTerm)) {
+                        return true;
+                    }
+                }));
+            }
         };
 
         // Init
-        self.getAdvancedSearchMetaData();
-
-        $('#left-column').hide();
-        $('#middle-column').addClass('expanded');
+        var searchText = document.getElementById("search-text");
+        searchText.addEventListener("keyup", self.searchForText, false);
     }
 })();
 
