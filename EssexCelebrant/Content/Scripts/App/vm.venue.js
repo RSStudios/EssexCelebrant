87f97e3dc74a977d29bc1f7cd22168222fda7dd0
Robert Silver <rs@rsstudios.co.uk>
2018-10-04 19:58:32 +0100

    Adding venues

---
+++ EssexCelebrant/Content/Scripts/App/vm.venue.js
@@ -0,0 +1,28 @@
+﻿(function () {
+
+    var EssexCelebrant = window.EssexCelebrant = window.EssexCelebrant || {};
+
+    EssexCelebrant.VenuesVM = function (venueData) {
+        var self = this;
+        self.venues = ko.observableArray(venueData);
+
+        self.clearSearchFilters = function () {
+         
+        };
+
+        self.runAdvancedSearch = function () {
+
+        };
+
+        self.getAdvancedSearchMetaData = function () {
+           
+        };
+
+        // Init
+        self.getAdvancedSearchMetaData();
+
+        $('#left-column').hide();
+        $('#middle-column').addClass('expanded');
+    }
+})();
+
