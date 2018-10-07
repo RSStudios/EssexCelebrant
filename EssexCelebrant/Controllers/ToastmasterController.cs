9c01acd17a021f37f9a30dd208c470b7267cba6b
Robert Silver <robsilveruk@gmail.com>
2018-10-07 12:47:58 +0100

    Undoing

---
+++ EssexCelebrant/Controllers/ToastmasterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EssexCelebrant.Controllers
+{
+    public class ToastmasterController : Controller
+    {
+        // GET: Toastmaster
+        public ActionResult Index()
+        {
+            return View("Toastmaster");
+        }
+    }
+}
