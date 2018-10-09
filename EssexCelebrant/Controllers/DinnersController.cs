e367f89445b87852aa399f3481cfa4c98b629501
Sally Bourne <rs@rsstudios.co.uk>
2018-10-09 21:45:20 +0100

    Added other celebrations and dinners views/controllers

---
+++ EssexCelebrant/Controllers/DinnersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EssexCelebrant.Controllers
+{
+    public class DinnersController : Controller
+    {
+        // GET: Dinners
+        public ActionResult Index()
+        {
+            return View("Dinners");
+        }
+    }
+}
---
+++ EssexCelebrant/Controllers/OtherCelebrationsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EssexCelebrant.Controllers
+{
+    public class OtherCelebrationsController : Controller
+    {
+        // GET: OtherCelebrations
+        public ActionResult Index()
+        {
+            return View("OtherCelebrations");
+        }
+    }
+}
