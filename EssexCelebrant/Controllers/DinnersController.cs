987b783d160d82d9471ac7144ec2a6cc4a02bae0
Robert Silver <robsilveruk@gmail.com>
2018-10-16 21:08:45 +0100

    Made CSS changes.  See Description.
    1) removed the carousel CSS from home.css. 2) added a new SASS file called carousel.scss. 3) wrote media queries for the carousel.

--- EssexCelebrant/Controllers/DinnersController.cs
+++ EssexCelebrant/Controllers/DinnersController.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace EssexCelebrant.Controllers
 {
     public class DinnersController : Controller
     {
-        [OutputCache(CacheProfile = "DefaultCache")]
-        // GET: Dinners
+        //[OutputCache(CacheProfile = "DefaultCache")]
         public ActionResult Index()
         {
             return View("Dinners");
--- EssexCelebrant/Controllers/OtherCelebrationsController.cs
+++ EssexCelebrant/Controllers/OtherCelebrationsController.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace EssexCelebrant.Controllers
 {
     public class OtherCelebrationsController : Controller
     {
-        // GET: OtherCelebrations
-        [OutputCache(CacheProfile = "DefaultCache")]
+       // [OutputCache(CacheProfile = "DefaultCache")]
         public ActionResult Index()
         {
             return View("OtherCelebrations");
