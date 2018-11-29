879c634929258bbc4f2f551b09cdbfde9feca77c
Unknown <robsilveruk@gmail.com>
2018-11-29 21:12:19 +0000

    Added a carousel to the Celebrant page and moved images around.

--- EssexCelebrant/Controllers/FuneralsController.cs
+++ EssexCelebrant/Controllers/FuneralsController.cs
@@ -4,7 +4,7 @@ namespace EssexCelebrant.Controllers
 {
     public class FuneralsController : Controller
     {
-        [OutputCache(CacheProfile = "DefaultCache")]
+        //[OutputCache(CacheProfile = "DefaultCache")]
         public ActionResult Index()
         {
             return View();
