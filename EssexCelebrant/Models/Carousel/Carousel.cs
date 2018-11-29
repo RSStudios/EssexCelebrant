1d6c7e92bb14649ce5c1682f201d7e9dfed35b8e
Unknown <robsilveruk@gmail.com>
2018-11-29 20:09:24 +0000

    See below
    Created a new carousel partial and then altered all of the files that use a carousel to make use of the partial.

---
+++ EssexCelebrant/Models/Carousel/Carousel.cs
@@ -0,0 +1,15 @@
+﻿namespace EssexCelebrant.Models.Carousel
+{
+    public class Carousel
+    {
+        public string Image { get; set; }
+
+        public string Line1 { get; set; }
+
+        public string Line2 { get; set; }
+
+        public bool Line1ContainsHtml { get; set; }
+
+        public bool Line2ContainsHtml { get; set; }
+    }
+}
