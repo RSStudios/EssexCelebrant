d9601c629306a8266057d0b186a5539558e1351e
Unknown <robsilveruk@gmail.com>
2018-12-16 12:48:46 +0000

    Put the cache back.

--- EssexCelebrant/Controllers/ToastmasterController.cs
+++ EssexCelebrant/Controllers/ToastmasterController.cs
@@ -4,7 +4,7 @@ namespace EssexCelebrant.Controllers
 {
     public class ToastmasterController : Controller
     {
-        //[OutputCache(CacheProfile = "DefaultCache")]
+        [OutputCache(CacheProfile = "DefaultCache")]
         public ActionResult Index()
         {
             return View("Toastmaster");
